b8e5ddaa1b98aeffa0bb376858b0173f3cf593c7
aguradev <wahmayun.wm@gmail.com>
2023-06-29 13:59:36 +0800

    register data

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,7 +13,7 @@ class RegisterController extends Controller
     {
         try {
             $data = [
-                "name" => $request->name,
+                "username" => $request->username,
                 "email" => $request->email,
                 "password" => Hash::make($request->password),
                 "joined_since" => now("GMT+8")
